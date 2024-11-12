86e8596998c8b324921571786c0ce7cfd1aebd66
Maxstasia <maxencestasiak@gmail.com>
2024-11-12 09:45:46 +0100

    add memchr.

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstasiak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 09:25:37 by mstasiak          #+#    #+#             */
+/*   Updated: 2024/11/12 09:44:48 by mstasiak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *str = (const unsigned char *)s;
+    size_t i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (str[i] == (unsigned char)c)
+            return ((void *)&str[i]);
+        i++;
+    }
+    return (NULL);
+}
