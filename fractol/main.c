6b7bd9f74e28d47dbb08f534463ad249597b5610
Maxstasia <maxencestasiak@gmail.com>
2024-12-16 09:38:55 +0100

    lets go push

--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: mstasiak <mstasiak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 18:23:44 by mstasiak          #+#    #+#             */
-/*   Updated: 2024/12/12 18:45:25 by mstasiak         ###   ########.fr       */
+/*   Updated: 2024/12/13 10:52:26 by mstasiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main()
 	ft_printf(" j'execute le programme \n");
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hello");
 	while(x ++, x < 250)
-		mlx_pixel_put(mlx_ptr, win_ptr, 0, 0, 552);
+		mlx_pixel_put(mlx_ptr, win_ptr, x, 0, 0xffffff);
 	ft_printf("test\n");
 	mlx_loop(mlx_ptr);
 	ft_printf(" j'ai fini \n");
