6e23e7644bdfd66d976de27f82969d31225e4e1d
Maxstasia <mstasiak@student.42.fr>
2025-03-25 16:16:53 +0100

    petite modif des makefile pour le flag -g et fin du Projet Pipex 	modified:   pipex/main_bonus.c

--- pipex/main_bonus.c
+++ pipex/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mstasiak <mstasiak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 14:35:53 by mstasiak          #+#    #+#             */
-/*   Updated: 2025/03/25 14:44:48 by mstasiak         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:07:32 by mstasiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,26 @@ static void	init_pipex(t_pipex *pipex, int argc, char **argv, char **envp)
 	pipex->prev_fd = -1;
 }
 
-static void	launch_processes(t_pipex *pipex, t_temp *tmp)
+static void launch_processes(t_pipex *pipex, t_temp *tmp)
 {
-	int	i;
+    int i;
 
-	i = -1;
-	while (++i < tmp->cmd_count)
-		tmp->last_pid = fork_process(pipex, i, tmp);
-	if (pipex->prev_fd != -1)
-		close(pipex->prev_fd);
-	while (wait(NULL) > 0 && errno != ECHILD)
-		;
-	waitpid(tmp->last_pid, &tmp->status, 0);
-	if (WIFEXITED(tmp->status))
-		tmp->last_status = WEXITSTATUS(tmp->status);
+    i = -1;
+    tmp->last_pid = -1;
+    while (++i < tmp->cmd_count)
+        tmp->last_pid = fork_process(pipex, i, tmp);
+    if (pipex->prev_fd != -1)
+        close(pipex->prev_fd);
+    while (wait(NULL) > 0 && errno != ECHILD)
+        ;
+    if (tmp->last_pid > 0)
+    {
+        tmp->status = 0;
+        if (waitpid(tmp->last_pid, &tmp->status, 0) > 0 && WIFEXITED(tmp->status))
+            tmp->last_status = WEXITSTATUS(tmp->status);
+        else
+            tmp->last_status = 1;
+    }
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -46,6 +52,7 @@ int	main(int argc, char **argv, char **envp)
 	if (argc < 5 || (ft_strcmp(argv[1], "here_doc") == 0 && argc < 6))
 		usage();
 	tmp.last_status = 0;
+	tmp.status = 0;
 	init_pipex(&pipex, argc, argv, envp);
 	if (pipex.here_doc)
 	{
@@ -53,9 +60,7 @@ int	main(int argc, char **argv, char **envp)
 		tmp.cmd_count = argc - 4;
 	}
 	else
-	{
 		tmp.cmd_count = argc - 3;
-	}
 	launch_processes(&pipex, &tmp);
 	return (tmp.last_status);
 }
