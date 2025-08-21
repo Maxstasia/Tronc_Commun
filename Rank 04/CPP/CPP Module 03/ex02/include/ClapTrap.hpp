43dd53fe540662f87572d476e9e158736c9d6921
Maxstasia <mstasiak@student.42.fr>
2025-08-21 17:55:37 +0200

    first step of the cpp module 03 ex02

---
+++ Rank 04/CPP/CPP Module 03/ex02/include/ClapTrap.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstasiak <mstasiak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 11:11:38 by mstasiak          #+#    #+#             */
+/*   Updated: 2025/08/21 17:35:41 by mstasiak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap {
+protected:
+	std::string		_name;
+	unsigned int	_hitPoints;
+	unsigned int	_energyPoints;
+	unsigned int	_attackDamage;
+
+public:
+	// Constructeurs
+	ClapTrap(void);
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& src);
+
+	// Destructeur
+	~ClapTrap(void);
+
+	
+
+	// Opérateur d'affectation
+	ClapTrap&		operator=(const ClapTrap& rhs);
+
+
+
+	// Méthodes
+	void			attack(const std::string& target);
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
+
+	// Getters
+	std::string		getName(void) const;
+	unsigned int	getHitPoints(void) const;
+	unsigned int	getEnergyPoints(void) const;
+	unsigned int	getAttackDamage(void) const;
+
+	// Setters
+	void			setName(const std::string& name);
+	void			setHitPoints(unsigned int hitPoints);
+	void			setEnergyPoints(unsigned int energyPoints);
+	void			setAttackDamage(unsigned int attackDamage);
+};
+
+// Surcharge d'opérateur chevron
+std::ostream&	operator<<(std::ostream& os, const ClapTrap& clapTrap);
+
+#endif
